fb21e2cd4bc314a09a6fbd5476f8510c68b1c796
madalina.toma <madalina.toma@softem.ro>
2022-11-27 15:46:37 +0200

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+             paralelipiped dreptunghic, va afisa volumul lui
+             */
+
+            int latime;
+            int lungime;
+            int inaltime;
+
+            Console.WriteLine("Introduceti latimea: ");
+            latime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti lungimea: ");
+            lungime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduceti inaltimea: ");
+            inaltime = int.Parse(Console.ReadLine());
+
+            int volumParalelipiped = latime * lungime * inaltime;
+            Console.WriteLine("Volumul este: " + volumParalelipiped);
+        }
+    }
+}
